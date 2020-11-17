1f8a9327695c50aef57aa3af1d426a8da0c7739b
Will Zeiher <wzeiher3@gmail.com>
2020-11-17 13:41:14 -0500

    Dashboard basics

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -4,21 +4,30 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Link } from 'react-router-dom';
 
-export default class MainPage extends React.Component{
+export default class Dashboard extends React.Component{
     state = {
         error: null,
     }
-}
+}   
 
 
 render(){
     return (
-        <section className="MainPage">
+        <section className="Dashboard">
             <div className="upperSection">
                  <div className="addTripButton">
                     
                  </div> 
+                 <div className="titleDiv">
+
+                 </div>
+                 <div className="myTripButton">
+
+                 </div>
             </div> 
+            <div className="lowerSection">
+                
+            </div>
         </section>
     )
 }
