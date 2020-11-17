54dada2a0053c99a1a7ef46592c7a203adda4f42
Will Zeiher <wzeiher3@gmail.com>
2020-11-17 11:21:57 -0500

    first push

---
+++ src/components/MainForm/MainPage.js
@@ -0,0 +1,24 @@
+'use strict';
+
+import React from 'react';
+import { render } from 'react-dom';
+import { Link } from 'react-router-dom';
+
+export default class MainPage extends React.Component{
+    state = {
+        error: null,
+    }
+}
+
+
+render(){
+    return (
+        <section className="MainPage">
+            <div className="upperSection">
+                 <div className="addTripButton">
+                    
+                 </div> 
+            </div> 
+        </section>
+    )
+}
