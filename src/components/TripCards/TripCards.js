0a92e85ac8c446aefdf6a90e7d36f7af669e3a76
unknown <richardcarmenscott@gmail.com>
2020-11-18 13:19:19 -0500

    changed font

--- src/components/TripCards/TripCards.js
+++ src/components/TripCards/TripCards.js
@@ -26,6 +26,7 @@ export default class TripCards extends Component {
   };
 
   render() {
+    let color = this.sequenceColorOnCard(this.props.index);
     let rating = ['*', '**', '***', '****', '*****'];
     return (
       <div className="TripCard">
@@ -36,15 +37,11 @@ export default class TripCards extends Component {
         <div className="TripCard-title">
           <h2>{this.props.destination}</h2>
         </div>
-        <div className="Categories">
-          <p>{this.props.short_description}</p>
+        <div className="Activities">
+          <span>{this.props.short_description}</span>
           <p>Activities: {this.props.activities}</p>
         </div>
-        <div
-          className={`TripCard-bottom ${this.sequenceColorOnCard(
-            this.props.index
-          )}`}
-        >
+        <div className={`TripCard-bottom ${color}`}>
           <div className="TripCard-bottom-info">
             Days {this.props.days} | Rating{' '}
             <span>{rating[this.props.rating - 1]}</span>
