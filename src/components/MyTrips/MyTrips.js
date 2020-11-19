82729ce118822d60b67a8331bca74feb4737115d
Mark Force <force.j.mark@gmail.com>
2020-11-19 15:09:22 -0500

    MyTrips is functional

--- src/components/MyTrips/MyTrips.js
+++ src/components/MyTrips/MyTrips.js
@@ -4,16 +4,15 @@ import TripCards from '../TripCards/TripCards'
 import TokenService from '../../services/token-service'
 import './MyTrips.css'
 
-
 export default class MyTrips extends Component {
     static contextType = TripContext;
 
     render() {
         let jwt = TokenService.getAuthToken()
         const user = TokenService.parseJwt(jwt)
-        // console.log(user)
+        
         const userTrips = this.context.trips.filter(trip => trip.user_id === user.user_id)
-        // console.log(userTrips)
+        
         const tripCards = userTrips.map((trip, index) => {
             return (
               <TripCards
@@ -32,7 +31,6 @@ export default class MyTrips extends Component {
         return (
             <section className="my-trips">
                 <div className="my-trips-header">
-
                 <h2>My Trips</h2>
                 <hr />
                 </div>
