57db7f5ae62b25224432277e535b6a739e24d250
unknown <richardcarmenscott@gmail.com>
2020-11-25 14:52:54 -0500

    added stop interactivity

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://ways-database-api.herokuapp.com/api',
+  //API_ENDPOINT: 'http://localhost:8000/api',
   TOKEN_KEY: 'blogful-client-auth-token',
   API_KEY: 'AIzaSyDTJKKolyKsSVxwQTVH6mh651l0SfClgrM',
-}
+};
