42b7ecaa9821c116a5ac006b9a340ae316bbc7c3
unknown <richardcarmenscott@gmail.com>
2020-12-04 17:53:55 -0500

    addtrip mobile reponsive

--- src/components/AddTripForm/AddTripForm.js
+++ src/components/AddTripForm/AddTripForm.js
@@ -229,11 +229,11 @@ export default class AddTripForm extends React.Component {
                 />
               </div>
               <br />
-              {/* <div className="flex"> */}
+              <div className="button-wrapper">
                 <button className="myButton" type="submit">
                   Submit!
                 </button>
-              {/* </div> */}
+              </div>
             </form>
           </div>
         </section>
